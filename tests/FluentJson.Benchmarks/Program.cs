8f75eef9e9e816f417f561b6e5e736200eb8c47f
Zakaria SMAHI <zakaria.smahi@gmail.com>
2026-03-16 01:49:10 +0100

    fix broken pipeline

--- tests/FluentJson.Benchmarks/Program.cs
+++ tests/FluentJson.Benchmarks/Program.cs
@@ -6,6 +6,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        BenchmarkRunner.Run<DeserializationBenchmarks>();
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
